8159a7ea84ce7cc30990299cd58e6ef38f8c788c
thekraalowais <thekra_alowais@hotmail.com>
2017-08-24 20:07:51 +0300

    third commit

--- app/src/main/java/com/example/thekra/readingtrackerapp/BookAdapter.java
+++ app/src/main/java/com/example/thekra/readingtrackerapp/BookAdapter.java
@@ -3,11 +3,14 @@ package com.example.thekra.readingtrackerapp;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+
+import com.example.thekra.readingtrackerapp.Contract.ReadingEntry;
 
 /**
  * Created by Thekra on 8/24/2017.
@@ -17,6 +20,7 @@ public class BookAdapter extends CursorAdapter {
     TextView name;
     TextView pages;
     TextView rate;
+    TextView id;
 
     public BookAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
@@ -30,19 +34,31 @@ public class BookAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView name = (TextView) view.findViewById(R.id.book_name);
-        TextView page = (TextView) view.findViewById(R.id.book_pages);
-        TextView rate = (TextView) view.findViewById(R.id.book_rate);
-        if (cursor.moveToNext()) {
-                String nameString = cursor.getString(cursor.getColumnIndex("name"));
-                int pageInt = cursor.getInt(cursor.getColumnIndex("page"));
-                int RateInt = cursor.getInt(cursor.getColumnIndex("Rating"));
+        id = (TextView) view.findViewById(R.id.book_name);
+        name = (TextView) view.findViewById(R.id.book_name);
+        pages = (TextView) view.findViewById(R.id.book_pages);
+        rate = (TextView) view.findViewById(R.id.book_rate);
+        if(cursor.moveToFirst())
+        do {
 
-                name.setText(nameString);
-                page.setText(String.valueOf(pageInt));
-                rate.setText(String.valueOf(RateInt));
-            }
+            int idInt = cursor.getColumnIndex(ReadingEntry._ID);
+            String nameString = cursor.getString(cursor.getColumnIndex(ReadingEntry.COLUMN_NAME));
+            int pageInt = cursor.getInt(cursor.getColumnIndex(ReadingEntry.COLUMN_PAGE));
+            int RateInt = cursor.getInt(cursor.getColumnIndex(ReadingEntry.COLUMN_RATING));
+            Log.v("SSSSSSSSSSSSSs", "id " + idInt);
+            Log.v("SSSSSSSSSSSSSs", "name " + nameString);
+            Log.v("SSSSSSSSSSSSSs", "page " + pageInt);
+            Log.v("SSSSSSSSSSSSSs", "rate " + RateInt);
 
+
+            name.setText(nameString);
+            pages.setText(String.valueOf(pageInt));
+            rate.setText(String.valueOf(RateInt));
+        }while(cursor.moveToNext());
+//        id.append(" " + idInt);
+//        name.append(" " + nameString);
+//        pages.append(" " + pageInt);
+//        rate.append(" " + RateInt);
     }
 }
 
